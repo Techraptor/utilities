7ed081c3b8ff7268317dd59c8954f18528962e31
falconraptor <falconraptors@gmail.com>
2015-06-15 12:17:22 -0700

    changed from scala and add a gitignore

---
+++ src/com/falconraptor/utilities/sql/SQL.java
@@ -0,0 +1,25 @@
+package com.falconraptor.utilities.sql;
+
+import java.sql.*;
+
+public class SQL {
+	public static final int mssql = 1;
+	public static final int mysql = 2;
+	private static SQL instance = null;
+	private Connection c = null;
+
+	public static SQL getInstance () {
+		if (instance == null) instance = new SQL();
+		return instance;
+	}
+
+	public void connect (String sql, String u, String p, int s) throws Exception {
+		if (s == mssql) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		else if (s == mysql) Class.forName("com.mysql.jdbc.Driver");
+		this.c = DriverManager.getConnection(sql, u, p);
+	}
+
+	public Statement getStatement () throws Exception {
+		return this.c.createStatement(1004, 1007);
+	}
+}
