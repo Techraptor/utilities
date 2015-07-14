fe7caa44280b5cc284d9f7c35983b8f0916d9dc9
falconraptor <falconraptors@gmail.com>
2015-07-13 22:43:08 -0700

    rewrote read and write to be instance based

--- src/com/falconraptor/utilities/files/Read.java
+++ src/com/falconraptor/utilities/files/Read.java
@@ -5,12 +5,40 @@ import com.falconraptor.utilities.logger.Logger;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.ArrayList;
 
 public class Read {
     private static final String log = "[com.falconraptor.utilities.files.Read.";
-    private static BufferedReader reader;
+    private BufferedReader reader;
 
-    public static boolean Read(String filename) {
+    public static ArrayList<String> readjar (String filename) {
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(Read.class.getClassLoader().getResourceAsStream(filename)));
+            String line = input.readLine();
+            ArrayList<String> out = new ArrayList<>(0);
+            while (line != null) {
+                out.add(line);
+                line = input.readLine();
+            }
+            input.close();
+            return out;
+        } catch (Exception e) {
+            Logger.logERROR(log + "readjar] " + e);
+            return null;
+        }
+    }
+
+    public static BufferedImage readImagefromJar (String filename) {
+        BufferedImage buff = null;
+        try {
+            buff = ImageIO.read(Read.class.getResourceAsStream(filename));
+        } catch (Exception e) {
+            Logger.logERROR(log + "readImagefromJar] " + e);
+        }
+        return buff;
+    }
+
+    public boolean Read (String filename) {
         try {
             File file = new File(filename);
             if (!file.exists()) {
@@ -25,46 +53,19 @@ public class Read {
         return true;
     }
 
-    public static String read() {
+    public ArrayList<String> read () {
         try {
             String line = reader.readLine();
-            String out = "";
+            ArrayList<String> out = new ArrayList<>(0);
             while (line != null) {
-                out += line + "↔";
+                out.add(line);
                 line = reader.readLine();
             }
             reader.close();
             return out;
         } catch (Exception e) {
             Logger.logERROR(log + "read] " + e);
-            return "";
+            return null;
         }
-    }
-
-    public static String readjar(String filename) {
-        try {
-            BufferedReader input = new BufferedReader(new InputStreamReader(Read.class.getClassLoader().getResourceAsStream(filename)));
-            String line = input.readLine();
-            String out = "";
-            while (line != null) {
-                out += line + "↔";
-                line = input.readLine();
-            }
-            input.close();
-            return out;
-        } catch (Exception e) {
-            Logger.logERROR(log + "readjar] " + e);
-            return "";
-        }
-    }
-
-    public static BufferedImage readImagefromJar(String filename) {
-        BufferedImage buff = null;
-        try {
-            buff = ImageIO.read(Read.class.getResourceAsStream(filename));
-        } catch (Exception e) {
-            Logger.logERROR(log + "readImagefromJar] " + e);
-        }
-        return buff;
     }
 }
--- src/com/falconraptor/utilities/logger/Logger.java
+++ src/com/falconraptor/utilities/logger/Logger.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Logger {
-    public static int level = 3;
     public static final Console console = new Console();
     private static final int ALL = 1;
     private static final int DEBUG = 3;
@@ -14,6 +13,18 @@ public class Logger {
     private static final int WARNING = 4;
     private static final int INFO = 2;
     private static final ArrayList<String> log = new ArrayList<>(0);
+    public static int level = 3;
+
+    public static void logALL (Object object) {
+        if (level <= ALL) log("[ALL] ", object);
+    }
+
+    private static void log (String level, Object object) {
+        String logitem = formatCalender(getCalendar()) + level + object.toString();
+        System.out.println(logitem);
+        log.add(logitem);
+        if (!console.closed) console.updateConsole(logitem);
+    }
 
     private static Calendar getCalendar() {
         return Calendar.getInstance();
@@ -21,10 +32,6 @@ public class Logger {
 
     private static String formatCalender(Calendar c) {
         return "[" + c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1) + "-" + c.get(Calendar.DAY_OF_MONTH) + "]" + " [" + c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND) + "] ";
-    }
-
-    public static void logALL(Object object) {
-        if (level <= ALL) log("[ALL] ", object);
     }
 
     public static void logDEBUG(Object object) {
@@ -43,16 +50,10 @@ public class Logger {
         if (level <= INFO) log("[INFO] ", object);
     }
 
-    private static void log(String level, Object object) {
-        String logitem = formatCalender(getCalendar()) + level + object.toString();
-        System.out.println(logitem);
-        log.add(logitem);
-        if (!console.closed) console.updateConsole(logitem);
-    }
-
     public static void saveLog(String file) {
-        Write.Write(file.substring(0, file.lastIndexOf(".")) + formatCalender(getCalendar()).substring(0, formatCalender(getCalendar()).lastIndexOf(":")).replaceAll(" ", "").replaceAll(":", ";") + "]" + file.substring(file.lastIndexOf(".")), false);
-        Write.write(log);
-        Write.close();
+        Write write = new Write();
+        write.Write(file.substring(0, file.lastIndexOf(".")) + formatCalender(getCalendar()).substring(0, formatCalender(getCalendar()).lastIndexOf(":")).replaceAll(" ", "").replaceAll(":", ";") + "]" + file.substring(file.lastIndexOf(".")), false);
+        write.write(log);
+        write.close();
     }
 }
